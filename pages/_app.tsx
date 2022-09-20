09b78732e72f45bd02d7a8abb0be45a5b5f55c2b
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-09-20 18:35:49 +0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+
+const darkTheme = createTheme({
+  type: 'dark',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
