a2624ef16d9ace4b6141d1e472cca5a616089495
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-09-26 18:29:22 +0530

    integrate checks api

--- pages/api/gh-webhook.ts
+++ pages/api/gh-webhook.ts
@@ -22,73 +22,100 @@ export default async function handler(
   const eventType = req.headers['x-github-event'];
 
   if (eventType === 'push') {
-    // backup the code to IPFS
-    const repoOwner = (req.body.repository.owner.name as string).toLowerCase();
+    const branchName = req.body.ref.split('/').pop();
+    const repoOwner = (req.body.repository.owner.login as string).toLowerCase();
     const repoName = (req.body.repository.name as string).toLowerCase();
     const installationId = req.body.installation.id;
     const commitSha = req.body.head_commit.id;
 
-    const repoContents = await fetch(
-      `https://api.github.com/repos/${repoOwner}/${repoName}/zipball`
-    );
-
-    const repoBuffer = await repoContents.buffer();
-    fs.writeFileSync('./repo.zip', repoBuffer);
-
-    // log the newly created zip file's size in megabytes
-    const stats = fs.statSync('./repo.zip');
-    const fileSizeInMegabytes = stats.size / 1000000.0;
-    console.debug(`repo.zip size: ${fileSizeInMegabytes} MB`);
-
-    const files = await getFilesFromPath('./repo.zip');
-    const cid = await web3StorageClient.put(files as Iterable<Filelike>);
-
-    console.log(`Successfully backed up to IPFS: ${cid}`);
-
-    // TODO: save to DB
-    const { error: dbError } = await supabase.from('backups').insert([
-      {
-        repo_owner: repoOwner,
-        repo_name: repoName,
-        backup_cid: cid,
-        commit_hash: commitSha,
-      },
-    ]);
-
-    console.log({
-      dbError,
-    });
-
-    // TODO: create deployment using github API
+    // create check run
     const octokit = getOctokitInstance(installationId);
     const { data } = await octokit.request(
-      'POST /repos/{owner}/{repo}/deployments',
+      'POST /repos/{owner}/{repo}/check-runs',
       {
         owner: repoOwner,
         repo: repoName,
-        ref: 'main',
-        payload: JSON.stringify({
-          backup_cid: cid,
-        }),
-        description: 'Backup deployment',
-        task: 'deploy',
+        name: 'Backup',
+        head_sha: commitSha,
+        status: 'in_progress',
+        started_at: new Date().toISOString(),
       }
     );
 
-    // @ts-expect-error
-    const deploymentId = data.id;
+    const checkRunId = data.id;
 
-    await octokit.request(
-      'POST /repos/{owner}/{repo}/deployments/{deployment_id}/statuses',
-      {
-        owner: repoOwner,
-        repo: repoName,
-        deployment_id: deploymentId,
-        state: 'success',
-        target_url: `https://ipfs.filebase.io/ipfs/${cid}`,
-        log_url: `https://ipfs.filebase.io/ipfs/${cid}`,
+    try {
+      // TODO: support custom branch names
+      if (branchName === 'main' || branchName === 'master') {
+        const repoContents = await fetch(
+          `https://api.github.com/repos/${repoOwner}/${repoName}/zipball`
+        );
+
+        const repoBuffer = await repoContents.buffer();
+        fs.writeFileSync('./repo.zip', repoBuffer);
+
+        // log the newly created zip file's size in megabytes
+        const stats = fs.statSync('./repo.zip');
+        const fileSizeInMegabytes = stats.size / 1000000.0;
+        console.debug(`repo.zip size: ${fileSizeInMegabytes} MB`);
+
+        const files = await getFilesFromPath('./repo.zip');
+        const cid = await web3StorageClient.put(files as Iterable<Filelike>);
+
+        console.log(`Successfully backed up to IPFS: ${cid}`);
+
+        const { error: dbError } = await supabase.from('backups').insert([
+          {
+            repo_owner: repoOwner,
+            repo_name: repoName,
+            backup_cid: cid,
+            commit_hash: commitSha,
+          },
+        ]);
+
+        if (dbError) {
+          console.error(dbError);
+        }
+
+        // mark check run as completed
+        await octokit.request(
+          'PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}',
+          {
+            owner: repoOwner,
+            repo: repoName,
+            check_run_id: checkRunId,
+            conclusion: 'success',
+            // @ts-expect-error
+            status: 'completed',
+            completed_at: new Date().toISOString(),
+            output: {
+              title: 'Backup',
+              summary: `Successfully backed up to IPFS: ${cid}`,
+            },
+          }
+        );
       }
-    );
+    } catch (error) {
+      console.error(error);
+
+      // mark check run as failed
+      await octokit.request(
+        'PATCH /repos/{owner}/{repo}/check-runs/{check_run_id}',
+        {
+          owner: repoOwner,
+          repo: repoName,
+          check_run_id: checkRunId,
+          conclusion: 'failure',
+          // @ts-expect-error
+          status: 'completed',
+          completed_at: new Date().toISOString(),
+          output: {
+            title: 'Backup',
+            summary: `Failed to backup to IPFS: ${error}`,
+          },
+        }
+      );
+    }
   }
 
   res.status(200).json({ name: 'John Doe' });
